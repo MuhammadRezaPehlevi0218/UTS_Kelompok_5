4e86806acac6d9e6d808a50ea6de530bd870b15f
MuhammadRezaPahlevi0218 <reyzae99@gmail.com>
2021-11-01 07:42:26 +0700

    Testing Side Navigation

--- cypress/integration/1941720230_MuhammadRezaPahlevi/navTesting.spec.js
+++ cypress/integration/1941720230_MuhammadRezaPahlevi/navTesting.spec.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-context('Login Testing', () => {
+context('Testing side bar 1941720230_MuhammadRezaPahlevi', () => {
   const username='standard_user'
   const password='secret_sauce'
   beforeEach(() => {
@@ -10,7 +10,34 @@ context('Login Testing', () => {
     cy.get(`#login-button`).should('be.visible').click();
   })
 
-  it('Data Log in Benar', () => {
-    
+  it('Testing Bugger Button 1941720230_MuhammadRezaPahlevi', () => {
+    cy.get(`#react-burger-menu-btn`).should('be.visible').click();
+    cy.get('#inventory_sidebar_link').should('be.visible');
+  })
+
+  it('Testing side bar all item 1941720230_MuhammadRezaPahlevi', () => {
+    cy.get(`#react-burger-menu-btn`).should('be.visible').click();
+    cy.get(`#inventory_sidebar_link`).should('be.visible').click();
+    cy.get(`#inventory_container`).should('be.visible');
+  })
+
+  it('Testing side bar About 1941720230_MuhammadRezaPahlevi', () => {
+    cy.get(`#react-burger-menu-btn`).should('be.visible').click();
+    cy.get(`#about_sidebar_link`).should('be.visible').click();
+    cy.get(`#entry-3qDFahnypj1KkiORyU1Zyh`).should('be.visible');
+  })
+
+  it('Testing side bar Logout 1941720230_MuhammadRezaPahlevi', () => {
+    cy.get(`#react-burger-menu-btn`).should('be.visible').click();
+    cy.get(`#logout_sidebar_link`).should('be.visible').click();
+    cy.get(`#login_button_container`).should('be.visible');
+  })
+
+  it('Testing side bar Reset App State 1941720230_MuhammadRezaPahlevi', () => {
+    cy.get(`#add-to-cart-sauce-labs-backpack`).should('be.visible').click();
+    cy.get(`#react-burger-menu-btn`).should('be.visible').click();
+    cy.get(`#reset_sidebar_link`).should('be.visible').click();
+    cy.get(`#shopping_cart_container`).should('be.visible').click();
+    cy.get(`cart_quantity`).should('1');
   })
 })
